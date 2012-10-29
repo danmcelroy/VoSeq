bd2fcce6ce54673c3e46ec7cfb144145028182c4
Carlos.Peña <mycalesis@gmail.com>
2012-10-29 03:16:34 +0200

    1.2.8 fixes in accents when creating users

--- login/admin-failed.php
+++ login/admin-failed.php
@@ -1,4 +1,4 @@
-<!-- 
+<?php
 // #################################################################################
 // #################################################################################
 // Voseq login/admin-failed.php
@@ -10,15 +10,14 @@
 //
 // Script overview: Print admin-failed page
 // #################################################################################
--->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<?php
+
 //check login session
 include'auth.php';
 //includes
 include'../conf.php';
 include'redirect.html';
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
