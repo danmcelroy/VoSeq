7e53866ff0f0b004a8cede3e0e96a13fa27075c0
Carlos Peña <mycalesis@gmail.com>
2013-01-30 23:47:14 +0200

    function get using curl

--- functions.php
+++ functions.php
@@ -400,4 +400,27 @@ function has_seqs($seqout_array, $gen) {
 		return "false";
 	}
 }
+
+
+// #################################################################################
+// Section: 
+// does a curl request
+// @input: $url string
+// @output: result using return transfer as string
+// #################################################################################
+function get_from_URL($url){
+	// use cURL, safer than simplexml_load_file
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+	$result = curl_exec($ch);
+
+	curl_close($ch);
+
+	return $result;
+}
+
 ?>
