fbcd8111ca9e72cbd0f3ab47aae811f7e6d36d92
Carlos Pena <mycalesis@gmail.com>
2013-01-30 16:21:29 +0200

    1.3.7

--- installation/install4.php
+++ installation/install4.php
@@ -125,7 +125,7 @@ session_start();\n
 \$pass = '" . $variables['pass']. "';
 \$db   = '" . $variables['db']  . "';\n
 \$config_sitename = '" . $variables['config_sitename'] . "';
-\$version = \"1.3.6\";
+\$version = \"1.3.7\";
 \$currentTemplate = 'templates/mytemplate';\n
 \$base_url = '" . $variables['url']. "';
 \$local_folder = '" . $variables['local_folder']. "';\n";
