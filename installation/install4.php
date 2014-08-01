80eedfa9e3d7f4aaf80572e6a17707cf49c75c0f
carlosp420 <mycalesis@gmail.com>
2014-08-01 16:23:22 +0300

    v1.7.4

--- installation/install4.php
+++ installation/install4.php
@@ -117,7 +117,7 @@ session_start();\n
 \$pass = '" . $variables['pass']. "';
 \$db   = '" . $variables['db']  . "';\n
 \$config_sitename = '" . $variables['config_sitename'] . "';
-\$version = \"1.7.3\";
+\$version = \"1.7.4\";
 \$currentTemplate = 'templates/mytemplate';\n
 \$base_url = '" . $variables['url']. "';
 \$local_folder = '" . $variables['local_folder']. "';\n";
