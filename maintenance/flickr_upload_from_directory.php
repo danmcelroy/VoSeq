2eaa53ff553894420674c880b3c9dc2d102fa9d5
Carlos Peña <mycalesis@gmail.com>
2014-08-19 13:56:39 +0300

    use spaces no tabs

--- maintenance/flickr_upload_from_directory.php
+++ maintenance/flickr_upload_from_directory.php
@@ -119,82 +119,81 @@ if (!$mysqli->real_connect($host, $user, $pass, $db)) {
 }
 
 echo "Success. Connected to MySQL... " . $mysqli->host_info . "\n";
-$mysqli->close();
-mysql_select_db($db) or die ('Unable to select database');
-mysql_query("set names utf8") or die("Error in query: " . mysql_error());
+$mysqli->query("set names utf8") or die("Error in query: " . $mysqli->error());
 
 $photos = array();
 foreach($di as $file) {
-	preg_match("/^(.+)\.png\$/", $file, $matches);
-	if( $matches ) {
-		$photos[] = $matches[1];
-	}
+    preg_match("/^(.+)\.png\$/", $file, $matches);
+    if( $matches ) {
+        $photos[] = $matches[1];
+    }
 }
 
+$mysqli->close();
 // upload pictures in the directory
 foreach( $photos as $item ) {
-	$query = "SELECT id, code, genus, species, subspecies, family, subfamily, tribe, subtribe, country, specificLocality, publishedIn, notes, voucherImage, latitude, longitude FROM ". $p_ . "vouchers WHERE code = \"$item\"";
-	$result = mysql_query($query) or die("Error in query: $query. " . mysql_error());
+    $query = "SELECT id, code, genus, species, subspecies, family, subfamily, tribe, subtribe, country, specificLocality, publishedIn, notes, voucherImage, latitude, longitude FROM ". $p_ . "vouchers WHERE code = \"$item\"";
+    $result = mysql_query($query) or die("Error in query: $query. " . mysql_error());
 
-	while( $row = mysql_fetch_object($result) ) {
-		$code = $row->code;
-		$genus = $row->genus;
-		$species = $row->species;
-		$subspecies = $row->subspecies;
-		$family = $row->family;
-		$subfamily = $row->subfamily;
-		$tribe = $row->tribe;
-		$subtribe = $row->subtribe;
-		$latitude = $row->latitude;
-		$longitude = $row->longitude;
-		if( $row->country != "" ) {
-			$country = "$row->country. ";
-		}
-		else {
-			$country = "";
-		}
+    while( $row = mysql_fetch_object($result) ) {
+        $code = $row->code;
+        $genus = $row->genus;
+        $species = $row->species;
+        $subspecies = $row->subspecies;
+        $family = $row->family;
+        $subfamily = $row->subfamily;
+        $tribe = $row->tribe;
+        $subtribe = $row->subtribe;
+        $latitude = $row->latitude;
+        $longitude = $row->longitude;
+        if( $row->country != "" ) {
+            $country = "$row->country. ";
+        }
+        else {
+            $country = "";
+        }
 
-		if( $row->specificLocality != "" ) {
-			$specificLocality = "$row->specificLocality, ";
-		}
-		else {
-			$specificLocality = "";
-		}
+        if( $row->specificLocality != "" ) {
+            $specificLocality = "$row->specificLocality, ";
+        }
+        else {
+            $specificLocality = "";
+        }
 
-		if( $row->publishedIn != "" ) {
-			$publishedIn = "$row->publishedIn, ";
-		}
-		else {
-			$publishedIn = "";
-		}
+        if( $row->publishedIn != "" ) {
+            $publishedIn = "$row->publishedIn, ";
+        }
+        else {
+            $publishedIn = "";
+        }
 
-		if( $row->notes != "" ) {
-			$notes = "$row->notes, ";
-		}
-		else {
-			$notes = "";
-		}
-	}
-		
-	$file = UPLOAD_DIRECTORY . "/" . $item . PHOTO_EXTENSION;
+        if( $row->notes != "" ) {
+            $notes = "$row->notes, ";
+        }
+        else {
+            $notes = "";
+        }
+    }
+        
+    $file = UPLOAD_DIRECTORY . "/" . $item . PHOTO_EXTENSION;
 
-	$photo_id = $f->sync_upload($file, "$code $genus $species $subspecies", "$country $specificLocality $publishedIn $notes", "$country,$family,$subfamily,$tribe,$subtribe,$genus,$species,$subspecies");
+    $photo_id = $f->sync_upload($file, "$code $genus $species $subspecies", "$country $specificLocality $publishedIn $notes", "$country,$family,$subfamily,$tribe,$subtribe,$genus,$species,$subspecies");
 
-	$info = $f->photos_getInfo($photo_id);
-	$my_voucherImage = $info['photo']['urls']['url'][0]['_content'];
-	$status = $f->photos_geo_setLocation($photo_id, $latitude, $longitude, "3");
-	$sizes = $f->photos_getSizes($photo_id);
-	
-	/*** create thumbnails ***/
-	foreach( $sizes as $i) {
-		foreach($i as $k => $v) {
-			if($k == "label" && $v == "Small") {
-				$my_url = $i['source'];
-			}
-		}
-	}
-	$query = "UPDATE ". $p_ . "vouchers set timestamp=now(), thumbnail=\"$my_url\", flickr_id=\"$photo_id\", voucherImage=\"$my_voucherImage\" where code=\"$item\""; 
-	echo $query . ";\n";
+    $info = $f->photos_getInfo($photo_id);
+    $my_voucherImage = $info['photo']['urls']['url'][0]['_content'];
+    $status = $f->photos_geo_setLocation($photo_id, $latitude, $longitude, "3");
+    $sizes = $f->photos_getSizes($photo_id);
+    
+    /*** create thumbnails ***/
+    foreach( $sizes as $i) {
+        foreach($i as $k => $v) {
+            if($k == "label" && $v == "Small") {
+                $my_url = $i['source'];
+            }
+        }
+    }
+    $query = "UPDATE ". $p_ . "vouchers set timestamp=now(), thumbnail=\"$my_url\", flickr_id=\"$photo_id\", voucherImage=\"$my_voucherImage\" where code=\"$item\""; 
+    echo $query . ";\n";
 #mysql_query($query) or die("Error in query: $query. " . mysql_error());
 }
 
