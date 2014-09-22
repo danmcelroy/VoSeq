4071635c3abd9637b25b7684fcbccea685b066b7
Carlos Peña <mycalesis@gmail.com>
2014-09-22 16:17:33 +0300

    django 1.7

---
+++ genomeseq/genomeseq/settings/local.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+from unipath import Path
+from django.core.exceptions import ImproperlyConfigured
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+BASE_DIR = Path(__file__).absolute().ancestor(3)
+SECRETS_FILE = os.path.join(BASE_DIR.ancestor(1), 'config.json')
+
+with open(SECRETS_FILE) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'leyes_sqlite3.db'),
+    }
+}
