312167b43ea87ab655ccfb8df317e4ee1ed2134e
Carlos Peña <mycalesis@gmail.com>
2014-09-26 16:48:45 +0300

    model Genes

--- genomeseq/public_interface/models.py
+++ genomeseq/public_interface/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Genes(models.Model):
+    gene_code = models.CharField(max_length=255)
+    genetic_code = models.PositiveSmallIntegerField(blank=True)
+    length = models.PositiveSmallIntegerField()
+    description = models.CharField(max_length=255, blank=True)
+    reading_frame = models.PositiveSmallIntegerField()
+    notes = models.TextField(blank=True)
+    aligned = models.CharField(max_length=6, choices=(
+        ('yes', 'yes'),
+        ('no', 'no'),
+        ('notset', 'notset'),
+    ),
+                               default='notset')
+    intron = models.CharField(max_length=255, blank=True)
+    prot_code = models.CharField(max_length=6, choices=(
+        ('yes', 'yes'),
+        ('no', 'no'),
+        ('notset', 'notset'),
+    ),
+                               default='notset')
+    gene_type = models.CharField(max_length=255, blank=True)
+
+    time_created = models.DateTimeField(auto_now_add=True)
