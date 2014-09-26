fc51bbc5f39164ebb8829066550f6cb8621d91a4
carlosp420 <mycalesis@gmail.com>
2014-09-26 12:41:42 +0300

    browse page

--- genomeseq/public_interface/views.py
+++ genomeseq/public_interface/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'public_interface/index.html')
+
+def browse(request):
+    return render(request, 'public_interface/browse.html')
