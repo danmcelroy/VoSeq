4aa7285f29abeac9bf7702cb14866afda559ec77
Carlos Peña <mycalesis@gmail.com>
2014-12-10 17:02:53 +0200

    show locality info in voucher page

--- genomeseq/public_interface/urls.py
+++ genomeseq/public_interface/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
     url(r'^browse$', views.browse, name='browse'),
+    url(r'^p/(?P<voucher_code>.+)/$', views.show_voucher, name='show_voucher'),
 )
--- genomeseq/public_interface/views.py
+++ genomeseq/public_interface/views.py
@@ -6,8 +6,16 @@ from .models import Vouchers
 def index(request):
     return render(request, 'public_interface/index.html')
 
+
 def browse(request):
     queryset = Vouchers.objects.all().order_by('-timestamp')[:10]
     return render(request, 'public_interface/browse.html',
                   {'results': queryset},
-    )
+                  )
+
+
+def show_voucher(request, voucher_code):
+    queryset = Vouchers.objects.get(code=voucher_code)
+    return render(request, 'public_interface/show_voucher.html',
+                  {'item': queryset},
+                  )
