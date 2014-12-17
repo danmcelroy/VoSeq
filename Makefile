9a3d7972dd5c46a66fa894686760b76c3a2106b2
Carlos Peña <mycalesis@gmail.com>
2014-12-17 11:25:35 +0200

    search does not work like icontains

--- Makefile
+++ Makefile
@@ -5,3 +5,5 @@ migrations:
 	cd voseq; python manage.py makemigrations --settings=voseq.settings.local
 	cd voseq; python manage.py migrate --settings=voseq.settings.local
 
+index:
+	cd voseq; python manage.py rebuild_index --settings=voseq.settings.local
--- voseq/public_interface/urls.py
+++ voseq/public_interface/urls.py
@@ -1,12 +1,29 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns
+from django.conf.urls import url
+from django.conf.urls import include
+
+from haystack.query import SearchQuerySet
+from haystack.views import SearchView
+from haystack.views import search_view_factory
 
 from . import views
+from .forms import AdvancedSearchForm
+
 
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
     url(r'^browse/$', views.browse, name='browse'),
     url(r'^search/$', views.search, name='search'),
+    # url(r'^search/', include('haystack.urls')),
     url(r'^p/(?P<voucher_code>.+)/$', views.show_voucher, name='show_voucher'),
     url(r'^s/(?P<voucher_code>.+)/(?P<gene_code>.+)/$', views.show_sequence, name='show_sequence'),
+)
+
+urlpatterns += patterns(
+    'haystack.views',
+    url(r'^$', SearchView(
+        template='search/search.html',
+        form_class=AdvancedSearchForm,
+    ), name='haystack_search'),
 )
