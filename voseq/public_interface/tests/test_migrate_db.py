5fc69656cf59c5ad4f8915e6c9700a5a73139450
carlosp420 <mycalesis@gmail.com>
2015-01-01 23:52:37 +0200

    test max, min altitude import

--- voseq/public_interface/tests/test_migrate_db.py
+++ voseq/public_interface/tests/test_migrate_db.py
@@ -2,6 +2,7 @@ from django.core.management import call_command
 from django.test import TestCase
 
 from public_interface.models import Vouchers
+from public_interface.models import Sequences
 
 
 class TestCustomCommand(TestCase):
@@ -62,3 +63,31 @@ class TestCustomCommand(TestCase):
     def test_genus(self):
         b = Vouchers.objects.get(code='CP100-09')
         self.assertEqual('Melitaea?', b.genus)
+
+    def test_accession(self):
+        b = Sequences.objects.get(code='CP100-10', gene_code='EF1a')
+        self.assertEqual('AY218269', b.accession)
+
+    def test_max_altitude_null_as_str(self):
+        b = Vouchers.objects.get(code='CP100-09')
+        self.assertEqual(None, b.max_altitude)
+
+    def test_max_altitude_null(self):
+        b = Vouchers.objects.get(code='CP100-11')
+        self.assertEqual(None, b.max_altitude)
+
+    def test_max_altitude_empty_with_space(self):
+        b = Vouchers.objects.get(code='CP100-10')
+        self.assertEqual(None, b.max_altitude)
+
+    def test_max_altitude1(self):
+        b = Vouchers.objects.get(code='CP100-12')
+        self.assertEqual(600, b.max_altitude)
+
+    def test_max_altitude2(self):
+        b = Vouchers.objects.get(code='CP100-13')
+        self.assertEqual(2500, b.max_altitude)
+
+    def test_max_altitude3(self):
+        b = Vouchers.objects.get(code='CP100-14')
+        self.assertEqual(2000, b.max_altitude)
