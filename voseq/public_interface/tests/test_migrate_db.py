c0e2ce2cc9a0962f43b1ba95153caa401058b9aa
Carlos Peña <mycalesis@gmail.com>
2015-01-01 16:41:24 +0200

    test family

--- voseq/public_interface/tests/test_migrate_db.py
+++ voseq/public_interface/tests/test_migrate_db.py
@@ -11,6 +11,30 @@ class TestCustomCommand(TestCase):
         cmd = 'migrate_db'
         call_command(cmd, *args, **opts)
 
+    def test_orden_none(self):
+        b = Vouchers.objects.get(code='CP100-09')
+        self.assertEqual('', b.orden)
+
+    def test_orden_null(self):
+        b = Vouchers.objects.get(code='CP100-10')
+        self.assertEqual('', b.orden)
+
+    def test_orden_space(self):
+        b = Vouchers.objects.get(code='CP100-12')
+        self.assertEqual('', b.orden)
+
     def test_superfamily(self):
         b = Vouchers.objects.get(code='CP100-10')
         self.assertEqual('', b.superfamily)
+
+    def test_family(self):
+        b = Vouchers.objects.get(code='CP100-09')
+        self.assertEqual('Nymphalidae', b.family)
+
+    def test_family_empty(self):
+        b = Vouchers.objects.get(code='CP100-10')
+        self.assertEqual('', b.family)
+
+    def test_family_space(self):
+        b = Vouchers.objects.get(code='CP100-11')
+        self.assertEqual('', b.family)
