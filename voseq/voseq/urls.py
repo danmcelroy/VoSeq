c1f0a427a078022d2ce380ab0cb4030a427a8f0b
Carlos Peña <mycalesis@gmail.com>
2015-01-03 01:46:43 +0200

    start blast local

--- voseq/voseq/urls.py
+++ voseq/voseq/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns(
     '',
+    url(r'^blast_local', include('blast_local.urls', namespace='blast_local')),
     url(r'^', include('public_interface.urls', namespace='public_interace')),
     # Examples:
     # url(r'^blog/', include('blog.urls')),
