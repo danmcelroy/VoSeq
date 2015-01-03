5775d03ab6487b9610569d787ddb22322cdac3c5
Carlos Peña <mycalesis@gmail.com>
2015-01-04 01:12:34 +0200

    test create blast db

--- voseq/blast_local/tests.py
+++ voseq/blast_local/tests.py
@@ -1,5 +1,7 @@
+import glob
 import os
 
+from django.core.management import call_command
 from django.test import TestCase
 from django.conf import settings
 
@@ -8,10 +10,16 @@ from .utils import BLAST
 
 class BlastLocalTest(TestCase):
     def setUp(self):
+        args = []
+        opts = {'dumpfile': 'test_db_dump.xml'}
+        cmd = 'migrate_db'
+        call_command(cmd, *args, **opts)
+
         blast_type = 'local'
         voucher_code = 'CP100-10'
         gene_code = 'COI'
         self.blast = BLAST(blast_type, voucher_code, gene_code)
+        self.seq_file = ''
 
     def test_have_blast_db(self):
         result = self.blast.have_blast_db()
@@ -35,7 +43,13 @@ class BlastLocalTest(TestCase):
         self.assertTrue(result)
 
     def test_create_blast_db(self):
+        self.blast.save_seqs_to_file()
         self.blast.create_blast_db()
-
-    def tearDown(self):
-        os.remove(self.seq_file)
+        files = glob.glob(
+            os.path.join(settings.BASE_DIR,
+                         '..',
+                         'blast_local',
+                         'db',
+                         'COI_seqs.fas.n*')
+        )
+        self.assertTrue(len(files) > 0)
