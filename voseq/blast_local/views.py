dc59451210b976daae0d942b30cc9a95b35495d0
Carlos Peña <mycalesis@gmail.com>
2015-01-03 18:32:23 +0200

    test save seqs to file

--- voseq/blast_local/views.py
+++ voseq/blast_local/views.py
@@ -1,26 +1,7 @@
-from Bio.Blast.Applications import NcbiblastnCommandline
+
 
 from django.shortcuts import render
 
 
 def index(request, voucher_code, gene_code):
     return render(request, 'public_interface/base.html')
-
-
-class BLAST(object):
-    def __init__(self, type, voucher_code, gene_code):
-        """
-        Type of blast to do: local, full, remote
-
-        :param type: local, full, remote.
-        :param voucher_code:
-        :param gene_code:
-        """
-        self.blast_type = type
-        self.voucher_code = voucher_code
-        self.gener_code = gene_code
-
-    def do_blast(self):
-        blastn_cline = NcbiblastnCommandline(query=self.query, db=self.db,
-                                             evalue=0.001, outfmt=5, out="opuntia.xml")
-        blastn_cline()
