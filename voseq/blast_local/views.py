e0543408e1cef8cb93f2ebd9664682eef9387a02
Carlos Peña <mycalesis@gmail.com>
2015-01-07 12:51:55 +0200

    Fixed arg verbosity->verbose and blast local

--- voseq/blast_local/views.py
+++ voseq/blast_local/views.py
@@ -1,7 +1,15 @@
-
-
 from django.shortcuts import render
+
+from .utils import BLAST
 
 
 def index(request, voucher_code, gene_code):
-    return render(request, 'public_interface/base.html')
+    blast = BLAST('local', voucher_code, gene_code, test=True)
+    blast.save_seqs_to_file()
+    blast.create_blast_db()
+    blast.save_query_to_file()
+    blast.do_blast()
+    result = blast.parse_blast_output()
+    print(result)
+    return render(request, 'blast_local/index.html',
+                  {'result': result})
