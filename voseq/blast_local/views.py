2a2ddf71f16bcaa297685df96a7e03ec0d31de1d
carlosp420 <mycalesis@gmail.com>
2015-01-09 23:13:30 +0200

    refactor blast view functions

--- voseq/blast_local/views.py
+++ voseq/blast_local/views.py
@@ -7,11 +7,8 @@ def index(request, voucher_code, gene_code):
     blast = BLAST('local', voucher_code, gene_code)
     blast.save_seqs_to_file()
 
-    if blast.have_blast_db() is False:
+    if blast.is_blast_db_up_to_date() is False:
         blast.create_blast_db()
-    else:
-        if blast.is_blast_db_up_to_date() is False:
-            blast.create_blast_db()
 
     blast.save_query_to_file()
     blast.do_blast()
