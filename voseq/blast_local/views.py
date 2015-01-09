7fe80a4183066fd6b456b2ab32f52ff54e72d65b
Carlos Peña <mycalesis@gmail.com>
2015-01-10 00:07:04 +0200

    blast: rm query and output file after being used

--- voseq/blast_local/views.py
+++ voseq/blast_local/views.py
@@ -13,5 +13,6 @@ def index(request, voucher_code, gene_code):
     blast.save_query_to_file()
     blast.do_blast()
     result = blast.parse_blast_output()
+    blast.delete_query_output_files()
     return render(request, 'blast_local/index.html',
                   {'result': result})
