68723edfcf19436f2d13c8c8d287e293819a6421
Carlos Peña <mycalesis@gmail.com>
2015-01-11 02:05:12 +0200

    test blast ncbi

--- voseq/blast_ncbi/tests.py
+++ voseq/blast_ncbi/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.core.management import call_command
 
-# Create your tests here.
+from blast_ncbi.utils import BLASTNcbi
+
+
+class TestNcbiBlast(TestCase):
+    def setUp(self):
+        args = []
+        opts = {'dumpfile': 'test_db_dump.xml', 'verbosity': 0}
+        cmd = 'migrate_db'
+        call_command(cmd, *args, **opts)
+
+        voucher_code = 'CP100-10'
+        gene_code = 'COI'
+        self.blast = BLASTNcbi(voucher_code, gene_code)
+
+    def test_blast_with_accession_number_in_header(self):
+        self.blast.save_query_to_file()
+        self.blast.do_blast()
+        result = self.blast.parse_blast_output()
+        self.blast.delete_query_output_files()
+        self.assertTrue(len(result) > 0)
