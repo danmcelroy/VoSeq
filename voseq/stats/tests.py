59b36cb1dd1a0a0e1d0289149fed19af1b6fa5db
carlosp420 <mycalesis@gmail.com>
2015-01-11 03:44:35 +0200

    test stats ncbi blast

--- voseq/stats/tests.py
+++ voseq/stats/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.core.management import call_command
 
-# Create your tests here.
+from stats.models import Stats
+
+
+class TestCustomCommand(TestCase):
+    def setUp(self):
+        args = []
+        opts = {'dumpfile': 'test_db_dump.xml', 'verbosity': 0}
+        cmd = 'migrate_db'
+        call_command(cmd, *args, **opts)
+
+    def test_create_stats(self):
+        call_command('create_stats')
+        res = Stats.objects.get(id=1)
+        expected = 10
+        self.assertEqual(expected, res.vouchers)
