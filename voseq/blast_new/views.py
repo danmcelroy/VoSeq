dc7414c673553a36f2def572bed3b4e051a160de
carlosp420 <mycalesis@gmail.com>
2015-01-16 13:42:46 +0200

    redirect if user uses GET onto results page

--- voseq/blast_new/views.py
+++ voseq/blast_new/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from stats.models import Stats
+from .forms import BLASTNewForm
+
+
+def index(request):
+    VERSION = settings.VERSION
+    try:
+        STATS = Stats.objects.get(pk=1)
+    except Stats.DoesNotExist:
+        STATS = ''
+    return render(request, 'blast_new/index.html',
+                  {
+                      'version': VERSION,
+                      'stats': STATS,
+                  },
+                  )
+
+
+def results(request):
+    if request.method == 'POST':
+        form = BLASTNewForm(request.POST)
+        if form.is_valid():
+            return render(request,
+                          'blast_new/results/')
+
+    return HttpResponseRedirect('/blast_new/')
