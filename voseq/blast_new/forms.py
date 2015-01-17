025bb47d4a5a3cd630d2877ba30a150022fec560
carlosp420 <mycalesis@gmail.com>
2015-01-17 12:39:20 +0200

    show available gene_codes

--- voseq/blast_new/forms.py
+++ voseq/blast_new/forms.py
@@ -1,13 +1,23 @@
 from django import forms
 
+from public_interface.models import Genes
+
 
 class BLASTNewForm(forms.Form):
     name = forms.CharField(
-        label='name',
+        label='Name',
         max_length=100,
     )
     sequence = forms.CharField(
-        label='sequence',
+        label='Sequence',
         required=True,
         widget=forms.Textarea,
+    )
+
+    queryset = Genes.objects.all()
+    CHOICES = [(i.gene_code, i.gene_code) for i in queryset]
+    gene_codes = forms.ChoiceField(
+        choices=CHOICES,
+        label='Genes',
+        widget=forms.CheckboxSelectMultiple(),
     )
