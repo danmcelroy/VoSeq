0b802f72cef5c0902feb62e188286f97b0924135
Carlos Peña <mycalesis@gmail.com>
2015-01-19 00:44:52 +0200

    it is voucher codes not gene_codes_list

--- voseq/genbank_fasta/urls.py
+++ voseq/genbank_fasta/urls.py
@@ -7,4 +7,6 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^/$', views.index, name='index'),
+    url(r'^/results/$', views.results, name='results'),
+
 )
--- voseq/genbank_fasta/views.py
+++ voseq/genbank_fasta/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 from core.utils import get_version_stats
 from .forms import GenBankFastaForm
@@ -16,3 +17,33 @@ def index(request):
                       'stats': stats,
                   },
                   )
+
+
+def results(request):
+    version, stats = get_version_stats()
+
+    if request.method == 'POST':
+        form = GenBankFastaForm(request.POST)
+
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            print(cleaned_data)
+            result = ''
+
+            return render(request, 'genbank_fasta/results.html',
+                          {
+                              'result': result,
+                              'version': version,
+                              'stats': stats,
+                          },
+                          )
+        else:
+            return render(request, 'genbank_fasta/index.html',
+                          {
+                              'form': form,
+                              'version': version,
+                              'stats': stats,
+                          },
+                          )
+
+    return HttpResponseRedirect('/genbank_fasta/')
