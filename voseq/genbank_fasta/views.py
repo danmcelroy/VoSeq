3bebb7fc6f99f4ade4a661ea2198ef5f29fe166d
Carlos Peña <mycalesis@gmail.com>
2015-01-18 23:35:05 +0200

    genbank fasta: form almost ready

--- voseq/genbank_fasta/views.py
+++ voseq/genbank_fasta/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+from core.utils import get_version_stats
+from .forms import GenBankFastaForm
+
 
 def index(request):
-    return render(request, 'genbank_fasta/index.html')
+    version, stats = get_version_stats()
+
+    form = GenBankFastaForm()
+    return render(request,
+                  'genbank_fasta/index.html',
+                  {
+                      'form': form,
+                      'version': version,
+                      'stats': stats,
+                  },
+                  )
