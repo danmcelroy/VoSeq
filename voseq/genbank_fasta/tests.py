91a52b0c65e821e5f887d710ff899e1730d01213
carlosp420 <mycalesis@gmail.com>
2015-01-20 00:42:13 +0200

    genbank fasta: test class, DNA fasta

--- voseq/genbank_fasta/tests.py
+++ voseq/genbank_fasta/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.core.management import call_command
 
-# Create your tests here.
+from genbank_fasta.utils import Results
+
+
+class ResultTest(TestCase):
+    def setUp(self):
+        args = []
+        opts = {'dumpfile': 'test_db_dump.xml', 'verbosity': 0}
+        cmd = 'migrate_db'
+        call_command(cmd, *args, **opts)
+
+        self.maxDiff = None
+
+    def test_fasta(self):
+        res = Results(['CP100-10', 'CP100-11'], ['COI'])
+        res.get_datasets()
+
+        expected = '>Melitaea_diamina_CP100-10 [org=Melitaea diamina] [Specimen-voucher=CP100-10] [note=cytochrome oxidase c subunit I gene, partial cds.] [Lineage=]\nTGAGCCGGTATAATTGGTACATCCCTAAGTCTTATTATTCGAACCGAATTAGGAAATCCTAGTTTTTTAATTGGAGATGATCAAATTTATAATACCATTGTAACAGCTCATGCTTTTATTATAATTTTTTTTATAGTTATGCCAATTATAATTGGAGGATTTGGTAATTGACTTGTACCATTAATATTGGGAGCCCCAGATATAGCTTTCCCCCGAATAAATTATATAAGATTTTGATTATTGCCTCCATCCTTAATTCTTTTAATTTCAAGTAGAATTGTAGAAAATGGGGCAGGAACTGGATGAACAGTTTACCCCCCACTTTCATCTAATATTGCCCATAGAGGAGCTTCAGTGGATTTAGCTATTTTTTCTTTACATTTAGCTGGGATTTCCTCTATCTTAGGAGCTATTAATTTTATTACTACAATTATTAATATACGAATTAATAATATATCTTATGATCAAATACCTTTATTTGTATGAGCAGTAGGAATTACAGCATTACTTCTCTTATTATCTTTACCAGTTTTAGCTGGAGCTATTACTATACTTTTAACGGATCGAAATCTTAATACCTCATTTTTTGATTCCTGCGGAGGAGGAGATCC\n'
+        self.assertEqual(expected, res.fasta)
