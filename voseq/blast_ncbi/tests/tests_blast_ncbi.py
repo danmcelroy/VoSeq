9c31cd99f7a24067e086c7e074e06029a169233e
Victor Solis <vm_solism@yahoo.es>
2015-01-25 19:56:17 +0100

    ready to test

---
+++ voseq/blast_ncbi/tests/tests_blast_ncbi.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.core.management import call_command
+
+from blast_ncbi.utils import BLASTNcbi
+
+
+class TestNcbiBlast(TestCase):
+    def setUp(self):
+        args = []
+        opts = {'dumpfile': 'test_db_dump.xml', 'verbosity': 0}
+        cmd = 'migrate_db'
+        call_command(cmd, *args, **opts)
+
+        voucher_code = 'CP100-10'
+        gene_code = 'COI'
+        self.blast = BLASTNcbi(voucher_code, gene_code)
+
+    def test_blast_with_accession_number_in_header(self):
+        self.blast.save_query_to_file()
+        self.blast.do_blast()
+        result = self.blast.parse_blast_output()
+        self.blast.delete_query_output_files()
+        self.assertTrue(len(result) > 0)
+
+    def test_index(self):
+        response = self.client.get('/blast_ncbi/CP100-10/COI/')
+        self.assertEqual(200, response.status_code)
---
+++ voseq/view_genes/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+
+from core.utils import get_version_stats
+from public_interface.models import Gene
+
+
+def index(request):
+    version, stats = get_version_stats()
+
+    queryset = Gene.objects.all()
+    return render(request, 'view_genes/index.html',
+                  {
+                      'result': queryset,
+                      'version': version,
+                      'stats': stats,
+                  },
+                  )
+
+
+def gene(request, gene_code):
+    version, stats = get_version_stats()
+
+    queryset = Gene.objects.filter(gene_code=gene_code)
+    if len(queryset) < 1:
+        item = ''
+    else:
+        item = queryset[0]
+
+        if ';' in item.intron:
+            introns = item.intron.split(';')
+            j = 1
+            out = ''
+            for i in introns:
+                out += '<b>Intron ' + str(j) + ':</b> ' + i + ' > '
+                j += 1
+            item.intron = out.rstrip(' > ')
+
+    return render(request, 'view_genes/gene.html',
+                  {
+                      'item': item,
+                      'version': version,
+                      'stats': stats,
+                  },
+                  )
