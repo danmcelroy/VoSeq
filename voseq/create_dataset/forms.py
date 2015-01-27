6022f54843045d68193f021f713c221b8e51497f
Carlos Peña <mycalesis@gmail.com>
2015-01-27 11:49:08 +0200

    keep adding form fields

--- voseq/create_dataset/forms.py
+++ voseq/create_dataset/forms.py
@@ -1,11 +1,9 @@
 from django import forms
 
-from public_interface.models import Genes
-from public_interface.models import GeneSets
-from public_interface.models import TaxonSets
+from core.forms import BaseDatasetForm
 
 
-class CreateDatasetForm(forms.Form):
+class CreateDatasetForm(BaseDatasetForm):
     file_format = forms.ChoiceField(
         label='Choose file format',
         choices=[
@@ -13,7 +11,8 @@ class CreateDatasetForm(forms.Form):
             ('NEXUS', 'NEXUS format'),
             ('PHY', 'PHYLIP format'),
             ('FASTA', 'Unaligned FASTA format'),
-        ]
+        ],
+        widget=forms.RadioSelect(),
     )
 
     outgroup = forms.CharField(
@@ -21,46 +20,25 @@ class CreateDatasetForm(forms.Form):
         help_text='Voucher code for using that specimen\'s sequence as '
                   'outgroup in NEXUS and TNT datasets.',
     )
-    """
-    taxonset = forms.ModelChoiceField(
-        TaxonSets.objects.all(),
-        label='Choose taxonset',
-        required=False,
-        empty_label='Choose taxonset',
-    )
-    geneset = forms.ModelChoiceField(
-        GeneSets.objects.all(),
-        label='Choose geneset',
-        required=False,
-        empty_label='Choose geneset',
-    )
-    gene_codes = forms.ModelMultipleChoiceField(
-        Genes.objects.all(),
-        label='Check to select your alignment/gene',
-        required=False,
+
+    positions = forms.ChoiceField(
+        label='Positions',
+        help_text='codon positions',
+        choices=[
+            ('ALL', 'all'),
+            ('1st', '1st'),
+            ('2nd', '2nd'),
+            ('3rd', '3rd'),
+        ],
         widget=forms.CheckboxSelectMultiple(),
     )
-    voucher_codes = forms.CharField(
-        widget=forms.Textarea,
-        label='... and/or a list of voucher codes',
-        required=False,
+
+    partition_by_positions = forms.ChoiceField(
+        label='Partition by positions',
+        choices=[
+            ('ONE', 'as one'),
+            ('EACH', 'each'),
+            ('1st-3rd', '1st-2nd, 3rd'),
+        ],
+        widget=forms.CheckboxSelectMultiple(),
     )
-    """
-
-    def clean(self):
-        """Overwriting validator method of class form."""
-        cleaned_data = super(GenBankFastaForm, self).clean()
-        taxonset = cleaned_data.get("taxonset")
-        voucher_codes = cleaned_data.get("voucher_codes")
-
-        geneset = cleaned_data.get("geneset")
-        gene_codes = cleaned_data.get("gene_codes")
-        print(gene_codes)
-
-        if taxonset is None and voucher_codes.strip() == '':
-            raise forms.ValidationError("You need to enter at least some "
-                                        "voucher codes or select a taxonset.")
-
-        if geneset is None and len(gene_codes) < 1:
-            raise forms.ValidationError("You need to enter at least some "
-                                        "gene codes or select a geneset.")
