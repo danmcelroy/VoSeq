37ab5d1b13625998df45d088ea696d45920c20f6
carlosp420 <mycalesis@gmail.com>
2015-01-29 11:34:05 +0200

    set initial value for file_format

--- voseq/create_dataset/forms.py
+++ voseq/create_dataset/forms.py
@@ -14,6 +14,7 @@ class CreateDatasetForm(BaseDatasetForm):
         ],
         widget=forms.RadioSelect(),
         required=True,
+        initial='FASTA',
     )
 
     outgroup = forms.CharField(
