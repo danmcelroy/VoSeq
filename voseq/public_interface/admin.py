64512e12d41485d54ab98219dd4975647593ec61
Victor Solis <vm_solism@yahoo.es>
2015-01-31 21:01:46 +0100

    Squashed PR #76 from mezarino
    correct field name of auctor for author
    
    admin site customization 2
    
    batch changes from admin site
    
    add admin templates path
    
    correct field name to author
    
    add form for batch changes
    
    correct field name to author
    
    add batch_changes template
    
    Update Makefile
    
    Delete 0001_initial.py

--- voseq/public_interface/admin.py
+++ voseq/public_interface/admin.py
@@ -1,6 +1,68 @@
 from django.contrib import admin
 
 from public_interface.models import Vouchers
+from django.http import HttpRequest
+from public_interface.views import change_selected
+from django.core.exceptions import PermissionDenied
+
+
+# Customize what and the way you show it
+# class VouchersAdmin(admin.ModelAdmin):
+
+
+class VouchersAdmin(admin.ModelAdmin):
+    list_display = ['code', 'genus', 'species', 'sex', 'voucher', 'country', 'collector']
+    ordering = ['code']
+    list_filter = ['voucher']
+    search_fields = ['=genus', '=species']
+    
+    #list_editable = ['genus', 'species', 'sex', 'voucher', 'country', 'collector']
+    
+    actions = ['batch_changes']
+
+    fieldsets = [('Voucher Information', {'fields': ['code', 'voucher', 'voucherLocality',
+                                                     'voucherCode','code_bold']}
+                  ),
+                 
+                 ('Specimen Information', {'fields': [('orden', 'superfamily',
+                                                      'family'), ('subfamily',
+                                                      'tribe', 'subtribe'),
+                                                      ('genus', 'species',
+                                                      'subspecies'), ('sex', 'typeSpecies'),
+                                                      ('author', 'determinedBy'), 'publishedIn'],
+                  'classes': ['collapse']}
+                  ),
+                 
+                 ('Collection Information', {'fields': [('country','specificLocality'),
+                                                        ('latitude','longitude'),
+                                                        ('max_altitude', 'min_altitude'),
+                                                        'dateCollection', ('collector',
+                                                        'hostorg'), 'dateExtraction',
+                                                        ('extractor','extraction',
+                                                         'extractionTube'), 'notes',
+                                                        'edits', 'latesteditor'],
+                  'classes': ['collapse']}
+                  ),
+                 
+                 (None, {'fields': ['timestamp']}),
+                 ]
+
+
+    def batch_changes(self, request, queryset):
+        # Check that the user has change permission for the actual model
+        if not self.has_change_permission(request):
+            raise PermissionDenied
+        else:
+            selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+            new_request = HttpRequest()
+            new_request.method = 'GET'
+            return change_selected(new_request, ",".join(selected))
+
+    batch_changes.short_description = "Change selected in batch"
+
 
 # Register your models here.
-admin.site.register(Vouchers)
+admin.site.register(Vouchers, VouchersAdmin)
+
+
+
