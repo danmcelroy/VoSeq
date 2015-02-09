3f35b22bcfcca3b62c89c1e5ef44c07698ead6b1
Carlos Peña <mycalesis@gmail.com>
2015-02-09 11:53:38 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -8,6 +8,27 @@ import voseq
 
 version = voseq.__version__
 
+requirements = [
+    'biopython',
+    'Django',
+    'pyprind',
+    'elasticsearch',
+    'Unipath',
+    'psycopg2',
+    'dataset',
+    'django-haystack',
+    'django-debug-toolbar',
+    'pytz',
+]
+
+test_requirements = [
+    'coverage',
+    'nose',
+    'pep8',
+    'coveralls',
+    'Sphinx',
+]
+
 setup(
     name='VoSeq',
     version=version,
@@ -17,9 +38,8 @@ setup(
         'voseq',
     ],
     include_package_data=True,
-    install_requires=[
-        'Django==1.7.3',
-    ],
+    install_requires=requirements,
     zip_safe=False,
     scripts=['voseq/manage.py'],
+    test_require=test_requirements,
 )
