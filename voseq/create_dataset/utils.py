089ce671aa7a017f0a97e135680b0e7caa2d7ea7
carlosp420 <mycalesis@gmail.com>
2015-02-15 19:06:34 +0200

    refactor 1st codon, one partition

--- voseq/create_dataset/utils.py
+++ voseq/create_dataset/utils.py
@@ -104,6 +104,30 @@ class CreateDataset(object):
             out += '\n'.join(partitions['codon1'])
             return out
 
+        if '1st' in self.codon_positions and \
+                '2nd' not in self.codon_positions and \
+                '3rd' not in self.codon_positions and \
+                'ALL' not in self.codon_positions and \
+                'ONE' in self.partition_by_positions:
+            for gene_code in self.seq_objs:
+                this_gene = None
+                for seq_record in self.seq_objs[gene_code]:
+
+                    if this_gene is None:
+                        this_gene = seq_record.name
+
+                        seq_str = '>' + this_gene + '_1st_codon\n' + '--------------------'
+                        partitions['codon1'].append(seq_str)
+
+                    seq_record_seqs = self.get_sequence_based_on_codon_positions(this_gene, seq_record.seq)
+
+                    seq_str = '>' + seq_record.id + '\n' + str(seq_record_seqs[0])
+                    partitions['codon1'].append(seq_str)
+
+            out = ''
+            out += '\n'.join(partitions['codon1'])
+            return out
+
         if '2nd' in self.codon_positions and \
                 '1st' not in self.codon_positions and \
                 '3rd' not in self.codon_positions and \
