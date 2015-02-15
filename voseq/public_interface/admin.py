e0dcda69e754c274f623f3b808caac452b71f26c
Carlos Peña <mycalesis@gmail.com>
2015-02-16 01:53:03 +0200

    pep8 admin

--- voseq/public_interface/admin.py
+++ voseq/public_interface/admin.py
@@ -1,52 +1,48 @@
 from django.contrib import admin
+from django.core.exceptions import PermissionDenied
+from django.http import HttpRequest
 
 from public_interface.models import Vouchers
-from django.http import HttpRequest
 from public_interface.views import change_selected
-from django.core.exceptions import PermissionDenied
 
 
 # Customize what and the way you show it
-# class VouchersAdmin(admin.ModelAdmin):
-
-
 class VouchersAdmin(admin.ModelAdmin):
     list_display = ['code', 'genus', 'species', 'sex', 'voucher', 'country', 'collector']
     ordering = ['code']
     list_filter = ['voucher']
     search_fields = ['=genus', '=species']
-    
-    #list_editable = ['genus', 'species', 'sex', 'voucher', 'country', 'collector']
-    
+
+    # list_editable = ['genus', 'species', 'sex', 'voucher', 'country', 'collector']
+
     actions = ['batch_changes']
 
     fieldsets = [('Voucher Information', {'fields': ['code', 'voucher', 'voucherLocality',
-                                                     'voucherCode','code_bold']}
+                                                     'voucherCode', 'code_bold']}
                   ),
-                 
-                 ('Specimen Information', {'fields': [('orden', 'superfamily',
-                                                      'family'), ('subfamily',
-                                                      'tribe', 'subtribe'),
-                                                      ('genus', 'species',
-                                                      'subspecies'), ('sex', 'typeSpecies'),
-                                                      ('author', 'determinedBy'), 'publishedIn'],
-                  'classes': ['collapse']}
-                  ),
-                 
-                 ('Collection Information', {'fields': [('country','specificLocality'),
-                                                        ('latitude','longitude'),
-                                                        ('max_altitude', 'min_altitude'),
-                                                        'dateCollection', ('collector',
-                                                        'hostorg'), 'dateExtraction',
-                                                        ('extractor','extraction',
-                                                         'extractionTube'), 'notes',
-                                                        'edits', 'latesteditor'],
-                  'classes': ['collapse']}
-                  ),
-                 
+
+                 ('Specimen Information', {'fields': ['orden', 'superfamily', 'family',
+                                                      'subfamily', 'tribe', 'subtribe',
+                                                      'genus', 'species', 'subspecies',
+                                                      'sex', 'typeSpecies',
+                                                      'author', 'determinedBy',
+                                                      'publishedIn',
+                                                      ],
+                                           'classes': ['collapse']}),
+
+                 ('Collection Information', {'fields': ['country', 'specificLocality',
+                                                        'latitude', 'longitude',
+                                                        'max_altitude', 'min_altitude',
+                                                        'dateCollection', 'collector',
+                                                        'hostorg', 'dateExtraction',
+                                                        'extractor', 'extraction',
+                                                        'extractionTube', 'notes',
+                                                        'edits', 'latesteditor',
+                                                        ],
+                                             'classes': ['collapse']}),
+
                  (None, {'fields': ['timestamp']}),
                  ]
-
 
     def batch_changes(self, request, queryset):
         # Check that the user has change permission for the actual model
@@ -63,6 +59,3 @@ class VouchersAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Vouchers, VouchersAdmin)
-
-
-
