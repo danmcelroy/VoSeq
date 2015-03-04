7d138030f10199a5bf67708fc29c737a49cc0f30
carlosp420 <mycalesis@gmail.com>
2015-03-04 12:02:29 +0200

    adding number of chars to TNT dataset

--- voseq/create_dataset/dataset.py
+++ voseq/create_dataset/dataset.py
@@ -6,12 +6,13 @@ class Dataset(object):
     """
     Base class to create datasets from Seq objects into FASTA, TNT formats.
     """
-    def __init__(self, codon_positions, partition_by_positions, seq_objs, gene_codes, file_format):
+    def __init__(self, codon_positions, partition_by_positions, seq_objs, gene_codes, voucher_codes, file_format):
         self.file_format = file_format
         self.codon_positions = codon_positions
         self.partition_by_positions = partition_by_positions
         self.seq_objs = seq_objs
         self.gene_codes = gene_codes
+        self.voucher_codes = voucher_codes
         self.reading_frames = self.get_reading_frames()
         self.warnings = []
 
