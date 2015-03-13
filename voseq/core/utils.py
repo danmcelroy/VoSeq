63567828bdf6a270d1539c029dca5f0ef0cbfaee
carlosp420 <mycalesis@gmail.com>
2015-03-13 13:13:12 +0200

    move method to core.utils

--- voseq/core/utils.py
+++ voseq/core/utils.py
@@ -5,6 +5,7 @@ import re
 from django.conf import settings
 
 from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
 
 from stats.models import Stats
 
@@ -199,3 +200,37 @@ def chain_and_flatten(seqs):
             append(i)
 
     return Seq(''.join(out))
+
+
+def translate_to_protein(gene_model, sequence_model, seq_description, seq_id):
+    # # Protein sequences
+    seq_seq, removed = strip_question_marks(sequence_model.sequences)
+    if int(gene_model.reading_frame) == 1:
+        if removed % 3 == 0:
+            start_translation = 0
+        if removed % 3 == 1:
+            start_translation = 2
+        if removed % 3 == 2:
+            start_translation = 1
+    if int(gene_model.reading_frame) == 2:
+        if removed % 3 == 0:
+            start_translation = 1
+        if removed % 3 == 1:
+            start_translation = 0
+        if removed % 3 == 2:
+            start_translation = 2
+    if int(gene_model.reading_frame) == 3:
+        if removed % 3 == 0:
+            start_translation = 2
+        if removed % 3 == 1:
+            start_translation = 1
+        if removed % 3 == 2:
+            start_translation = 0
+    if '?' in seq_seq or 'N' in seq_seq.upper():
+        seq_obj = Seq(seq_seq[start_translation:], IUPAC.ambiguous_dna)
+    else:
+        seq_obj = Seq(seq_seq[start_translation:], IUPAC.unambiguous_dna)
+    prot_sequence = seq_obj.translate(table=gene_model.genetic_code)
+    out = '>' + seq_id + ' ' + seq_description + '\n'
+    out += str(prot_sequence) + '\n'
+    return out
