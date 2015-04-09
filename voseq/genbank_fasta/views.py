4a68babf917b98057ab32c6433e6197619882b12
carlosp420 <mycalesis@gmail.com>
2015-04-09 15:01:28 +0300

    complete refactoring of genbank_fasta

--- voseq/genbank_fasta/views.py
+++ voseq/genbank_fasta/views.py
@@ -10,6 +10,7 @@ from core.utils import get_version_stats
 from core.utils import get_gene_codes
 from core.utils import get_voucher_codes
 from .forms import GenBankFastaForm
+from create_dataset.utils import CreateDataset
 from . import utils
 
 
@@ -36,27 +37,33 @@ def results(request):
 
         if form.is_valid():
             cleaned_data = form.cleaned_data
+            cleaned_data['file_format'] = 'GenbankFASTA'
+            cleaned_data['number_genes'] = ''
+            cleaned_data['aminoacids'] = True
+            cleaned_data['positions'] = 'ALL'
+            cleaned_data['partition_by_positions'] = 'ONE'
+            cleaned_data['taxon_names'] = ['CODE', 'GENUS', 'SPECIES']
+            cleaned_data['outgroup'] = ''
 
-            voucher_codes = get_voucher_codes(cleaned_data)
-            gene_codes = get_gene_codes(cleaned_data)
+            dataset_creator = CreateDataset(cleaned_data)
+            dataset = dataset_creator.dataset_str
+            errors = dataset_creator.errors
+            warnings = dataset_creator.warnings
 
-            res = utils.Results(voucher_codes, gene_codes)
-            res.get_datasets()
-            items_with_accession = res.items_with_accession
-            fasta = res.fasta[0:1500] + '\n...\n\n\n' + '#######\nComplete dataset file available for download.\n#######'
-            protein = res.protein[0:1500] + '\n...\n\n\n' + '#######\nComplete dataset file available for download.\n#######'
-            fasta_file = re.search('(fasta_[a-z0-9]+\.fasta)', res.fasta_file).groups()[0]
-            protein_file = re.search('(prot_[a-z0-9]+\.fasta)', res.protein_file).groups()[0]
-            warnings = res.warnings
+            dataset_file_abs = dataset_creator.dataset_file
+            if dataset_file_abs is not None:
+                dataset_file = re.search('([A-Z]+_[a-z0-9]+\.txt)', dataset_file_abs).groups()[0]
+            else:
+                dataset_file = False
 
             return render(request, 'genbank_fasta/results.html',
                           {
-                              'items_with_accession': items_with_accession,
-                              'fasta': fasta,
-                              'fasta_file': fasta_file,
-                              'protein': protein,
-                              'protein_file': protein_file,
-                              'warnings': warnings,
+                              'items_with_accession': '',
+                              'fasta': '',
+                              'fasta_file': dataset_file,
+                              'protein': '',
+                              'protein_file': '',
+                              'warnings': '',
                               'version': version,
                               'stats': stats,
                           },
