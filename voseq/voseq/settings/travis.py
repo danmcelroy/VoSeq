1226ac5ebfb22303f86aade90c91c6cfb3a299b3
carlosp420 <mycalesis@gmail.com>
2015-04-28 15:20:39 +0300

    minimize travis.py setting

--- voseq/voseq/settings/travis.py
+++ voseq/voseq/settings/travis.py
@@ -3,18 +3,8 @@ To be used in Travis CI so we can skip running the tests for NCBI blast, which
 fails most of the time due to network problems.
 """
 
-import sys
-
-from .base import *
+from .testing import *
 
 
-print('Testing in Travis')
-TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
+print('in Travis')
 TRAVIS = True
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'test.db',
-    }
-}
