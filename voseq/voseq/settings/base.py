ab37b73a415dd63cf057c379f28df0f9edc3ea46
Carlos Peña <mycalesis@gmail.com>
2015-04-28 13:54:12 +0300

    dont use key_secret in base.py file

--- voseq/voseq/settings/base.py
+++ voseq/voseq/settings/base.py
@@ -16,9 +16,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hola'
-
 
 ALLOWED_HOSTS = []
 
--- voseq/voseq/settings/testing.py
+++ voseq/voseq/settings/testing.py
@@ -4,6 +4,9 @@ from .base import *
 
 
 print('Testing')
+
+SECRET_KEY = 'hola'
+
 TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
 
 DATABASES = {
