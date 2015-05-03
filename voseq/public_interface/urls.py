9addfed1ac404296596163a8a2b2fb4847f1b838
carlosp420 <mycalesis@gmail.com>
2015-05-03 10:21:15 +0300

    fix url for advanced search

--- voseq/public_interface/urls.py
+++ voseq/public_interface/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns(
     url(r'^$', views.index, name='index'),
     url(r'^browse/$', views.browse, name='browse'),
     url(r'^search/$', views.search, name='simple_search'),
+    url(r'^search/advanced/$', views.search_advanced, name='advanced_search'),
     url(r'^p/(?P<voucher_code>.+)/$', views.show_voucher, name='show_voucher'),
     url(r'^s/(?P<voucher_code>.+)/(?P<gene_code>.+)/$', views.show_sequence, name='show_sequence'),
 
