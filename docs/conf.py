affefa08b1ff187086c7885908643a61e909c123
Carlos Peña <mycalesis@gmail.com>
2015-05-04 16:02:56 +0300

    fix spacing in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,19 +10,18 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
-
-import sys, os
+import sys
+import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
 
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.append(os.path.abspath('_themes'))
 
-
 import voseq
+
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'voseq.voseq.settings.base'
 
