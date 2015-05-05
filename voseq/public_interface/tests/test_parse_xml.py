04737c64163e8516f619e6b6835b32c09eec8dd5
Carlos Peña <mycalesis@gmail.com>
2015-05-05 15:03:09 +0300

    test migrate_db

---
+++ voseq/public_interface/tests/test_parse_xml.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from public_interface.management.commands.migrate_db import ParseXML
+
+
+class TestParseXML(TestCase):
+    def setUp(self):
+        with open('test_db_dump.xml') as handle:
+            self.xml_string = handle.read()
+
+        self.parse_xml = ParseXML(xml_string=self.xml_string, tables_prefix='voseq_',
+                                  verbosity=0)
+
+    def test_parse_table_genes(self):
+        """We don't have tables with prefix 'voseq_' in our test_db_dump.xml file
+        """
+        self.assertRaises(ValueError, self.parse_xml.parse_table_genes, self.xml_string)
