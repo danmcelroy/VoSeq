84576fdad56c3b68966477f9cb99edbc04c32032
Carlos Peña <mycalesis@gmail.com>
2015-05-06 10:47:45 +0300

    renaming indexes

--- voseq/public_interface/search_indexes.py
+++ voseq/public_interface/search_indexes.py
@@ -7,6 +7,8 @@ from .models import Sequences
 
 
 class SimpleSearchIndex(indexes.SearchIndex, indexes.Indexable):
+    """We have only taxonomical fields for this search.
+    """
     text = indexes.EdgeNgramField(document=True, use_template=True)
     code = indexes.EdgeNgramField(model_attr='code')
     orden = indexes.EdgeNgramField(model_attr='orden', null=True)
@@ -29,13 +31,12 @@ class SimpleSearchIndex(indexes.SearchIndex, indexes.Indexable):
         return self.get_model().objects.filter(timestamp__lte=datetime.datetime.now())
 
 
-class AdvancedSearchIndex(SimpleSearchIndex):
+class VouchersIndex(SimpleSearchIndex):
     """Defines an index and all fields should have autocomplete capabilities
     in the GUI.
 
     :param indexes.SearchIndex:
     :param indexes.Indexable:
-    :returns: class AdvancedSearchIndex
     """
     text = indexes.CharField(document=True, use_template=True)
     author = indexes.EdgeNgramField(model_attr='author', null=True)
@@ -54,12 +55,14 @@ class AdvancedSearchIndex(SimpleSearchIndex):
     publishedIn = indexes.EdgeNgramField(model_attr='publishedIn', null=True)
     notes = indexes.EdgeNgramField(model_attr='notes', null=True)
 
-    def index_queryset(self, using='advanced_search'):
+    def index_queryset(self, using='vouchers'):
         # Used when the entire index for model is updated.
         return self.get_model().objects.filter(timestamp__lte=datetime.datetime.now())
 
 
-class SequencesIndex(indexes.SearchIndex, indexes.Indexable):
+class AdvancedSearchIndex(indexes.SearchIndex, indexes.Indexable):
+    """We also need some fields from our Sequences model.
+    """
     text = indexes.EdgeNgramField(document=True, use_template=True)
     labPerson = indexes.EdgeNgramField(model_attr='labPerson', null=True)
     accession = indexes.EdgeNgramField(model_attr='accession', null=True)
@@ -67,6 +70,6 @@ class SequencesIndex(indexes.SearchIndex, indexes.Indexable):
     def get_model(self):
         return Sequences
 
-    def index_queryset(self, using='sequences'):
+    def index_queryset(self, using='advanced_search'):
         # Used when the entire index for model is updated.
         return self.get_model().objects.filter(time_created__lte=datetime.datetime.now())
