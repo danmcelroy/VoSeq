467b6238d646f6353f72ba5ec336305232ef0ad7
carlosp420 <mycalesis@gmail.com>
2015-05-12 10:55:49 +0300

    show list of genes in advanced search form

--- voseq/public_interface/forms.py
+++ voseq/public_interface/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from haystack.forms import ModelSearchForm
 from haystack.query import SearchQuerySet
 
+from public_interface.models import Genes
+
 
 class AdvancedSearchForm(ModelSearchForm):
     NULL = 'Select'
@@ -90,7 +92,12 @@ class AdvancedSearchForm(ModelSearchForm):
         (YES, 'Yes'),
         (NO, 'No'),
     )
-    gene_code = forms.CharField(max_length=100, required=False)
+    gene_code = forms.ModelChoiceField(
+        Genes.objects.all().order_by('gene_code'),
+        required=False,
+        widget=forms.Select(),
+        empty_label='Select',
+    )
     genbank = forms.ChoiceField(widget=forms.RadioSelect, choices=GENBANK_CHOICES, required=False)
     accession = forms.CharField(max_length=100, required=False)
     labPerson = forms.CharField(max_length=100, required=False)
