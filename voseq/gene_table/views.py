4dd4302a44e7608aae0d94a388b4e1eedc8ff4f9
carlosp420 <mycalesis@gmail.com>
2015-05-15 21:38:47 +0300

    getting all data needed for table

--- voseq/gene_table/views.py
+++ voseq/gene_table/views.py
@@ -10,6 +10,7 @@ from core.utils import get_version_stats
 from core.utils import get_voucher_codes
 from core.utils import get_gene_codes
 from create_dataset.utils import CreateDataset
+from public_interface.models import Genes
 
 
 def index(request):
@@ -45,6 +46,7 @@ class GeneTable(object):
         self.gene_codes = get_gene_codes(cleaned_data)
         self.fasta_dataset = self.get_fasta_dataset()
         self.fasta_partitions = self.split_in_partitions()
+        self.genes_type = self.get_genes_type()
         self.stats = self.get_stats_from_partitions()
 
     def populate_cleaned_data_form(self, cleaned_data):
@@ -77,22 +79,19 @@ class GeneTable(object):
 
         return partitions
 
+    def get_genes_type(self):
+        genes = Genes.objects.all().values('gene_code', 'gene_type')
+        genes_dict = {}
+        for gene in genes:
+            gene_code = gene['gene_code']
+            gene_type = gene['gene_type']
+            genes_dict[gene_code] = gene_type
+        return genes_dict
+
     def get_stats_from_partitions(self):
         """These are the stats headers of AMAS v0.2
-
-        dna_header = [
-            "Alignment_name",
-            "No_of_taxa",
-            "Alignment_length",
-            "Total_matrix_cells",
-            "Undetermined_characters",
-            "Missing_percent",
-            "No_variable_sites",
-            "Proportion_variable_sites",
-            "Parsimony_informative_sites",
-            "Proportion_parsimony_informative",
-        ]
         """
+        stats = {}
         in_file = self.make_guid() + '.fas'
 
         for code, partition in self.fasta_partitions.items():
@@ -100,13 +99,27 @@ class GeneTable(object):
                 handle.write(partition)
 
             aln = AMAS.DNAAlignment(in_file, 'fasta', 'dna')
-            print(aln.summarize_alignment())
-            print(code, partition)
+            aln_stats = aln.summarize_alignment()
+
+            this_stat = {
+                'data_type': self.genes_type[code],
+                'number_of_taxa': aln_stats[1],
+                'alignment_length': aln_stats[2],
+                'total_matrix_cells': aln_stats[3],
+                'undetermined_chars': aln_stats[4],
+                'missing_percent': aln_stats[5],
+                'number_variable_sites': aln_stats[6],
+                'proportion_variable_sites': aln_stats[7],
+                'parsimony_informative_sites': aln_stats[8],
+                'proportion_parsimony_informative': aln_stats[9],
+            }
+            stats[code] = this_stat
 
         try:
             os.remove(in_file)
         except OSError:
             pass
+        return stats
 
     def make_guid(self):
         return uuid.uuid4().hex
