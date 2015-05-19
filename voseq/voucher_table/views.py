31a6c1d28d0db1e34f1cb186f74dfa572e894896
Carlos Peña <mycalesis@gmail.com>
2015-05-19 09:55:40 +0300

    start building form

--- voseq/voucher_table/views.py
+++ voseq/voucher_table/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from collections import OrderedDict
+import csv
+import os
+import uuid
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from amas import AMAS
+
+from .forms import VoucherTableForm
+from core.utils import get_version_stats
+from core.utils import get_voucher_codes
+from core.utils import get_gene_codes
+from create_dataset.utils import CreateDataset
+from public_interface.models import Genes
+
+
+def index(request):
+    version, stats = get_version_stats()
+    form = VoucherTableForm()
+    print(form)
+
+    return render(request, 'voucher_table/index.html',
+                  {
+                      'version': version,
+                      'stats': stats,
+                      'form': form,
+                  },
+                  )
+
+
+def results(request):
+    pass
