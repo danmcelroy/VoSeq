d24f181717de8c6b5f129f328971ebf1e38cb418
Carlos Peña <mycalesis@gmail.com>
2015-05-19 09:55:40 +0300

    start building form

---
+++ voseq/voucher_table/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^/$', views.index, name='index'),
+    url(r'^/results/$', views.results, name='results'),
+)
