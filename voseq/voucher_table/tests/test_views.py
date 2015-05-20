a686a1a7aa77556a5aef333a16620fcbb427af20
carlosp420 <mycalesis@gmail.com>
2015-05-20 10:45:25 +0300

    testing views

---
+++ voseq/voucher_table/tests/test_views.py
@@ -0,0 +1,26 @@
+from django.test import Client
+from django.test import TestCase
+from django.core.management import call_command
+
+from public_interface.models import GeneSets
+from public_interface.models import TaxonSets
+from voucher_table.views import VoucherTable
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        args = []
+        opts = {'dumpfile': 'test_db_dump.xml', 'verbosity': 0}
+        cmd = 'migrate_db'
+        call_command(cmd, *args, **opts)
+
+        self.client = Client()
+        self.maxDiff = None
+
+    def test_index(self):
+        response = self.client.get('/create_voucher_table/')
+        self.assertEqual(200, response.status_code)
+
+    def test_result_redirect(self):
+        response = self.client.get('/create_voucher_table/results/')
+        self.assertEqual(302, response.status_code)
