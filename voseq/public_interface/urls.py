7d0c965515911196cd2cdc9762c86b1d4f4e54c2
carlosp420 <mycalesis@gmail.com>
2015-05-27 14:37:34 +0300

    test not working

--- voseq/public_interface/urls.py
+++ voseq/public_interface/urls.py
@@ -15,5 +15,5 @@ urlpatterns = patterns(
     url(r'^s/(?P<voucher_code>.+)/(?P<gene_code>.+)/$', views.show_sequence, name='show_sequence'),
 
     # for admin purposes
-    url(r'^admin/public_interface/vouchers/batch_changes/ids=(?P<selected>[CP\d+\-\d+\,*]*)/$', views.change_selected, name='change_selected'),
+    url(r'^admin/public_interface/vouchers/batch_changes/ids=(?P<selected>.+)/$', views.change_selected, name='change_selected'),
 )
