9a70945f13096efecd9606de0a5e03db3767247c
carlosp420 <mycalesis@gmail.com>
2015-05-27 11:07:03 +0300

    install django-registration-redux

--- voseq/accounts/views.py
+++ voseq/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.core.context_processors import csrf
+from django.contrib.auth.views import login
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'accounts/login.html')
+    # url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
