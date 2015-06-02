e9ca3e67cff4c6acaf3061d06d6b288f73592416
carlosp420 <mycalesis@gmail.com>
2015-06-02 14:27:53 +0300

    rm date from version string

--- voseq/voseq/settings/base.py
+++ voseq/voseq/settings/base.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import re
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -168,7 +170,7 @@ def get_version():
             if 'xx' in line:
                 continue
             elif 'Version' in line or 'release' in line.lower():
-                return line
+                return re.sub('\(.+\)', '', line)
     else:
         return 'xyz'
 VERSION = get_version()
