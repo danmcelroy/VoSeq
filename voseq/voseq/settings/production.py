f67c0a85a09288db9a95c326b9935bb7e482cb2d
Carlos Peña <mycalesis@gmail.com>
2015-06-12 14:38:56 +0300

    showing voucher photos in voucher page, with config updates

--- voseq/voseq/settings/production.py
+++ voseq/voseq/settings/production.py
@@ -1,10 +1,12 @@
 from .local import *
 
-STATIC_ROOT = "/www/VoSeq/static/"
+# you need to specify a public folder your your statics files during deployment
+STATIC_ROOT = "/var/www/VoSeq/static/"
+MEDIA_ROOT = "/var/www/VoSeq/media/"
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = [
-    '192.168.0.106',  # Domain or IP address
+    '192.168.0.106',  # Your Domain or IP address
 ]
