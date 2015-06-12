12b8d698140a014bfca612f355232301b8ac44ea
Carlos Peña <mycalesis@gmail.com>
2015-06-12 14:38:56 +0300

    showing voucher photos in voucher page, with config updates

--- voseq/voseq/urls.py
+++ voseq/voseq/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = patterns(
@@ -19,4 +21,4 @@ urlpatterns = patterns(
 
     # user auth urls
     url(r'^accounts/', include('registration.backends.default.urls')),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
