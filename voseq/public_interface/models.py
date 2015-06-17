a11adaf73b66cd99112fdcf7f38304203a0e7f7e
Carlos Peña <mycalesis@gmail.com>
2015-06-17 15:53:44 +0300

    saving total number of bp for sequences in table

--- voseq/public_interface/models.py
+++ voseq/public_interface/models.py
@@ -173,7 +173,7 @@ class Sequences(models.Model):
     time_edited = models.DateTimeField(auto_now=True, null=True, blank=True)
     notes = models.TextField(blank=True)
     genbank = models.BooleanField()
-    # total_number_bp = models.IntegerField(blank=True, null=True)
+    total_number_bp = models.IntegerField(blank=True, null=True)
     number_ambiguous_bp = models.IntegerField(blank=True, null=True)
 
     class Meta:
@@ -183,8 +183,7 @@ class Sequences(models.Model):
         ambiguous_seq_length = self.sequences.count('?') + self.sequences.count('-')
         ambiguous_seq_length += self.sequences.count('N') + self.sequences.count('n')
         self.number_ambiguous_bp = ambiguous_seq_length
-        # TODO save length of sequence string as *total_number_bp*
-        # self.total_number_bp = len(self.sequences)
+        self.total_number_bp = len(str(self.sequences))
         super(Sequences, self).save(*args, **kwargs)
 
     def __str__(self):
