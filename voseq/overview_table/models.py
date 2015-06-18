8065558aa185ae3f97b1e1b744f7d6235acee4b3
Carlos Peña <mycalesis@gmail.com>
2015-06-18 11:05:19 +0300

    fix model for overview tables, not allow code?

--- voseq/overview_table/models.py
+++ voseq/overview_table/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
-from public_interface.models import Vouchers
 
-
-class OverviewTable(Vouchers):
-    sequences = models.TextField(help_text="HTML string of cells with length of"
-                                           "sequences for each gene.")
+class OverviewTable(models.Model):
+    """Bulk create does not work on inherited models so we need to create a new
+    one.
+    """
+    sequence_string = models.TextField(help_text="HTML string of cells with length of"
+                                                 "sequences for each gene.")
+    o_code = models.CharField(max_length=300)
+    orden = models.TextField(blank=True)
+    superfamily = models.TextField(blank=True)
+    family = models.TextField(blank=True)
+    subfamily = models.TextField(blank=True)
+    genus = models.TextField(blank=True)
+    species = models.TextField(blank=True)
