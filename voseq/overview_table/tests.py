21e59c22247a342ff26689135470538292541683
carlosp420 <mycalesis@gmail.com>
2015-06-18 15:33:02 +0300

    using datatables dev 1.10.8 as the previous version is buggy

---
+++ voseq/overview_table/tests.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.core.management import call_command
+
+from overview_table.utils import OverviewTableMaker
+
+
+class TestOverviewTable(TestCase):
+
+    def setUp(self):
+        args = []
+        opts = {'dumpfile': 'test_db_dump.xml', 'verbosity': 0}
+        cmd = 'migrate_db'
+        call_command(cmd, *args, **opts)
+
+    def test_utils(self):
+        o = OverviewTableMaker()
+        expected = {
+            'orden': '',
+            'family': '',
+            'code': 'CP1',
+        }
+        self.assertEqual(expected, o.items)
