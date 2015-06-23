6b8b21ae836218eb28518c0c69d0e6d4c8aa0d71
Carlos Peña <mycalesis@gmail.com>
2015-06-23 10:40:06 +0300

    fix serving media files in development

--- voseq/voseq/urls.py
+++ voseq/voseq/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles import views
+
 
 urlpatterns = patterns(
     '',
@@ -23,3 +25,8 @@ urlpatterns = patterns(
     # user auth urls
     url(r'^accounts/', include('registration.backends.default.urls')),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^/media/(?P<path>.*)$', views.serve)
+    ]
