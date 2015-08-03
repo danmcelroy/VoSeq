a95e4165a657c8347d4560ab36f8d5e00f3bc47f
Carlos Peña <mycalesis@gmail.com>
2015-08-03 14:53:37 +0300

    degenerate MEGA

--- voseq/create_dataset/utils.py
+++ voseq/create_dataset/utils.py
@@ -56,7 +56,8 @@ class CreateDataset(object):
         if self.file_format == 'MEGA':
             fasta = CreateMEGA(self.codon_positions, self.partition_by_positions,
                                self.seq_objs, self.gene_codes, self.voucher_codes,
-                               self.file_format)
+                               self.file_format, aminoacids=self.aminoacids,
+                               degen_translations=self.degen_translations, translations=self.translations)
             fasta_dataset = fasta.from_seq_objs_to_dataset()
             self.warnings += fasta.warnings
             self.dataset_file = fasta.dataset_file
