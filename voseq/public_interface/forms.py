10ae1082e6f08f025b4f82b4e8edcda76b7e01af
carlosp420 <mycalesis@gmail.com>
2015-08-13 10:29:36 +0300

    show error message when trying to save invalid chars as sequences

--- voseq/public_interface/forms.py
+++ voseq/public_interface/forms.py
@@ -2,13 +2,11 @@ import datetime
 from functools import partial
 
 from django import forms
-from django.contrib import messages
 from haystack.forms import ModelSearchForm
 from haystack.query import SearchQuerySet
 from Bio.Alphabet import IUPAC
 
 from public_interface.models import Genes
-from core.exceptions import InvalidNucleotidae
 
 
 DateInput = partial(forms.DateInput, {'class': 'datepicker form-control',
@@ -20,9 +18,12 @@ class SequencesAdminForm(forms.ModelForm):
         valid_letters = set(IUPAC.ambiguous_dna.letters.upper() + 'N?-')
         sequence = str(self.cleaned_data['sequences'])
         for nucleotide in sequence:
-            if not valid_letters.issuperset(nucleotide.upper()):
-                raise InvalidNucleotidae("The character {} is not valid. "
-                                         "Cannot save this sequence".format(nucleotide))
+            if nucleotide == ' ':
+                self.add_error('sequences', "White spaces are not valid. "
+                                            "Cannot save this sequence")
+            elif not valid_letters.issuperset(nucleotide.upper()):
+                self.add_error('sequences', "The character {} is not valid. "
+                                            "Cannot save this sequence".format(nucleotide))
         return sequence
 
 
