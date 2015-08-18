c5ccdb76ffd98914f2b75555649614c09b398eee
carlosp420 <mycalesis@gmail.com>
2015-08-18 12:24:20 +0300

    test phylip aa dataset

--- voseq/create_dataset/tests/create_phylip_dataset/tests_create_phylip_dataset2.py
+++ voseq/create_dataset/tests/create_phylip_dataset/tests_create_phylip_dataset2.py
@@ -34,6 +34,10 @@ class CreatePhylipDatasetTest(TestCase):
             'aminoacids': False,
             'outgroup': '',
         }
+        self.dataset_file = os.path.join(settings.BASE_DIR, '..', 'create_dataset',
+                                         'tests', 'create_phylip_dataset', 'dataset.phy')
+        self.aa_dataset_file = os.path.join(settings.BASE_DIR, '..', 'create_dataset',
+                                            'tests', 'create_phylip_dataset', 'aa_dataset.phy')
 
         self.user = User.objects.get(username='admin')
         self.user.set_password('pass')
@@ -41,7 +45,7 @@ class CreatePhylipDatasetTest(TestCase):
 
         self.c = Client()
         self.dataset_creator = CreateDataset(self.cleaned_data)
-        # self.maxDiff = None
+        self.maxDiff = None
 
     def test_create_simple_dataset(self):
         dataset_file = os.path.join(settings.BASE_DIR, '..', 'create_dataset',
@@ -52,9 +56,43 @@ class CreatePhylipDatasetTest(TestCase):
         self.assertEqual(expected, result)
 
     def test_charset_block_file_of_simple_dataset(self):
-        dataset_file = os.path.join(settings.BASE_DIR, '..', 'create_dataset',
-                                    'tests', 'create_phylip_dataset', 'phylip_file.txt')
-        with open(dataset_file, "r") as handle:
+        with open(self.dataset_file, "r") as handle:
             expected = handle.read()
         result = self.dataset_creator.charset_block
+        self.assertEqual(expected, result)
+
+    def test_create_aa_dataset(self):
+        with open(self.aa_dataset_file, "r") as handle:
+            expected = handle.read()
+
+        cleaned_data = self.cleaned_data.copy()
+        cleaned_data['aminoacids'] = True
+        dataset_creator = CreateDataset(cleaned_data)
+
+        result = dataset_creator.dataset_str
+        self.assertEqual(expected, result)
+
+    def test_stop_codon_warning(self):
+        self.c.post('/accounts/login/', {'username': 'admin', 'password': 'pass'})
+        c = self.c.post('/create_dataset/results/',
+                        {
+                            'voucher_codes': '',
+                            'gene_codes': [],
+                            'geneset': 1,
+                            'taxonset': 1,
+                            'translations': False,
+                            'introns': 'YES',
+                            'file_format': 'PHY',
+                            'degen_translations': 'NORMAL',
+                            'exclude': 'YES',
+                            'aminoacids': True,
+                            'special': False,
+                            'outgroup': '',
+                            'positions': 'ALL',
+                            'partition_by_positions': 'ONE',
+                            'taxon_names': ['CODE', 'GENUS', 'SPECIES'],
+                        }
+                        )
+        expected = 'stop'
+        result = str(c.content)
         self.assertEqual(expected, result)
