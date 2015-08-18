552bd41e07c44bfa806214cd2ed150441594ecab
Carlos Peña <mycalesis@gmail.com>
2015-08-18 12:24:20 +0300

    test phylip aa dataset

--- voseq/create_dataset/tests/create_phylip_dataset/tests_create_phylip_dataset.py
+++ voseq/create_dataset/tests/create_phylip_dataset/tests_create_phylip_dataset.py
@@ -39,42 +39,6 @@ class CreatePhylipDatasetTest(TestCase):
         self.dataset_creator = CreateDataset(self.cleaned_data)
         self.maxDiff = None
 
-    def test_create_dataset(self):
-        expected = '2 761\nCP100-10_Melitaea_diamina'
-        result = self.dataset_creator.dataset_str
-        self.assertTrue(expected in result)
-
-    def test_only_one_set_of_taxon_names(self):
-        expected = 1
-        dataset = self.dataset_creator.dataset_str
-        result = dataset.count('CP100-10_Melitaea_diamina')
-        self.assertEqual(expected, result)
-
-    def test_stop_codon_warning(self):
-        self.c.post('/accounts/login/', {'username': 'admin', 'password': 'pass'})
-        c = self.c.post('/create_dataset/results/',
-                        {
-                            'voucher_codes': '',
-                            'gene_codes': [],
-                            'geneset': 1,
-                            'taxonset': 1,
-                            'translations': False,
-                            'introns': 'YES',
-                            'file_format': 'PHY',
-                            'degen_translations': 'NORMAL',
-                            'exclude': 'YES',
-                            'aminoacids': True,
-                            'special': False,
-                            'outgroup': '',
-                            'positions': 'ALL',
-                            'partition_by_positions': 'ONE',
-                            'taxon_names': ['CODE', 'GENUS', 'SPECIES'],
-                        }
-                        )
-        expected = 'stop'
-        result = str(c.content)
-        self.assertTrue(expected in result)
-
     def test_numer_of_chars_for_aa_dataset(self):
         self.c.post('/accounts/login/', {'username': 'admin', 'password': 'pass'})
         c = self.c.post('/create_dataset/results/',
