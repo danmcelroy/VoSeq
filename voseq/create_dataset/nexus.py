d36814fd1135a543db9009cb41e218f5014911aa
carlosp420 <mycalesis@gmail.com>
2015-08-25 14:57:07 +0300

    fix charsetblock nexus

---
+++ voseq/create_dataset/nexus.py
@@ -0,0 +1,121 @@
+import collections
+
+from .dataset import Dataset
+from public_interface.models import Genes
+
+
+class CreateNEXUS(Dataset):
+    def get_charset_block(self):
+        charset_block = []
+
+        bp_count_start = 0
+        bp_count_end = 0
+        self.gene_codes.sort()
+        for gene in self.gene_codes_and_lengths:
+            bp_count_end += self.gene_codes_and_lengths[gene]
+            line = '    charset ' + gene + ' = ' + str(
+                bp_count_start + 1) + '-' + str(bp_count_end) + ';'
+            bp_count_start += self.gene_codes_and_lengths[gene]
+            charset_block.append(line)
+        return charset_block
+
+    def get_partitions_block(self):
+        line = 'partition GENES = ' + str(len(self.gene_codes_and_lengths))
+        line += ': ' + ', '.join([i for i in self.gene_codes_and_lengths]) + ';\n'
+        line += '\nset partition = GENES;\n'
+        return [line]
+
+    def get_final_block(self):
+        block = "set autoclose=yes;"
+        if self.outgroup != '':
+            block += '\noutgroup ' + self.voucher_codes_metadata[self.outgroup] + ";"
+        block += """
+prset applyto=(all) ratepr=variable brlensp=unconstrained:Exp(100.0) shapepr=exp(1.0) tratiopr=beta(2.0,1.0);
+lset applyto=(all) nst=mixed rates=gamma [invgamma];
+unlink statefreq=(all);
+unlink shape=(all) revmat=(all) tratio=(all) [pinvar=(all)];
+mcmc ngen=10000000 printfreq=1000 samplefreq=1000 nchains=4 nruns=2 savebrlens=yes [temp=0.11];
+ sump relburnin=yes [no] burninfrac=0.25 [2500];
+ sumt relburnin=yes [no] burninfrac=0.25 [2500] contype=halfcompat [allcompat];
+END;
+"""
+        return [block.strip()]
+
+    def convert_lists_to_dataset(self, partitions):
+        """
+        Overridden method from base class in order to add headers and footers depending
+        on needed dataset.
+        """
+        self.get_number_of_genes_for_taxa(partitions)
+        self.get_number_chars_from_partition_list(partitions)
+
+        gene_models = Genes.objects.all().values()
+        gene_codes_and_lengths = collections.OrderedDict()
+
+        partitions_incorporated = 0
+
+        out = []
+
+        for partition in partitions:
+            for i in partition:
+                voucher_code = i.split(' ')[0]
+                if voucher_code.startswith('\n'):
+                    ThisGeneAndPartition = self.get_gene_for_current_partition(
+                        gene_models, out, partitions_incorporated, voucher_code
+                    )
+                    out = ThisGeneAndPartition.out
+                elif voucher_code.startswith('>'):
+                    voucher_code = i.splitlines()[0].replace('>', '')
+                    sequence = i.splitlines()[1]
+                    i = '{} {}'.format(voucher_code, sequence)
+                    ThisGeneAndPartition = self.get_gene_for_current_partition(
+                        gene_models, out, partitions_incorporated, voucher_code
+                    )
+
+                if voucher_code not in self.vouchers_to_drop:
+                    line = i.split(' ')
+                    if len(line) > 1:
+                        sequence = line[-1]
+
+                        if self.aminoacids is True:
+                            sequence = self.translate_this_sequence(
+                                sequence,
+                                ThisGeneAndPartition.this_gene_model,
+                                voucher_code
+                            )
+                        if self.aminoacids is not True:
+                            sequence = self.degenerate(sequence, ThisGeneAndPartition.this_gene_model)
+
+                        gene_codes_and_lengths[ThisGeneAndPartition.this_gene] = len(sequence)
+
+                        tmp_seq = sequence.replace('-', '')
+                        if len(tmp_seq) < 1:
+                            out = ['\n[{}]'.format(voucher_code)]
+                        else:
+                            out += [line[0].ljust(55, ' ') + sequence]
+
+        number_chars = 0
+        for k in gene_codes_and_lengths.keys():
+            number_chars += gene_codes_and_lengths[k]
+
+        header = [
+            '#NEXUS\n',
+            'BEGIN DATA;',
+            'DIMENSIONS NTAX=' + str(self.number_taxa - len(self.vouchers_to_drop)) + ' NCHAR=' + str(number_chars) + ';',
+            'FORMAT INTERLEAVE DATATYPE=DNA MISSING=? GAP=-;',
+            'MATRIX',
+        ]
+
+        if self.aminoacids is True:
+            self.gene_codes_and_lengths = gene_codes_and_lengths
+
+        out = header + out
+
+        out += [';\nEND;']
+        out += ['\nbegin mrbayes;']
+        out += self.get_charset_block()
+        out += self.get_partitions_block()
+        out += self.get_final_block()
+        dataset_str = '\n'.join(out)
+        self.save_dataset_to_file(dataset_str)
+        return dataset_str
