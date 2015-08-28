517d9d6a79ab49238d93525eabdba2485ba8abba
carlosp420 <mycalesis@gmail.com>
2015-08-28 10:25:10 +0300

    fix 1st2nd dataset

--- voseq/create_dataset/tests/create_nexus_dataset/tests_create_nexus_dataset.py
+++ voseq/create_dataset/tests/create_nexus_dataset/tests_create_nexus_dataset.py
@@ -221,6 +221,20 @@ class CreateNexusDatasetTest(TestCase):
         result = dataset_creator.dataset_str
         self.assertEqual(expected.strip(), result)
 
+    def test_nexus_1st_2nd_codon_as_each(self):
+        dataset_file = os.path.join(settings.BASE_DIR, '..', 'create_dataset',
+                                    'tests', 'create_nexus_dataset', 'dataset_1st2nd_codons_partitioned_as_each.nex')
+        with open(dataset_file, 'r') as handle:
+            expected = handle.read()
+
+        cleaned_data = self.cleaned_data.copy()
+        cleaned_data['positions'] = ['1st', '2nd']
+        cleaned_data['partition_by_positions'] = 'EACH'
+        dataset_creator = CreateDataset(cleaned_data)
+
+        result = dataset_creator.dataset_str
+        self.assertEqual(expected.strip(), result)
+
     def test_nexus_with_outgroup(self):
         cleaned_data = self.cleaned_data
         cleaned_data['outgroup'] = 'CP100-11'
