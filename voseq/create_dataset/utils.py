894baf33e277421bd80d45b1cd6c87515d9438f0
carlosp420 <mycalesis@gmail.com>
2015-10-19 16:03:52 +0300

    fixes #306

--- voseq/create_dataset/utils.py
+++ voseq/create_dataset/utils.py
@@ -134,9 +134,7 @@ class CreateDataset(object):
             return tnt_dataset
 
         if self.file_format in ['NEXUS', 'FASTA']:
-            if self.partition_by_positions == 'ONE':
-                self.partition_by_positions = 'by gene'
-            elif self.partition_by_positions == 'EACH' or 'EACH' in self.partition_by_positions:
+            if self.partition_by_positions == 'EACH' or 'EACH' in self.partition_by_positions:
                 self.partition_by_positions = 'by codon position'
             elif self.partition_by_positions == '1st2nd_3rd':
                 self.partition_by_positions = '1st-2nd, 3rd'
