171fb613cd338ddad13b94a68981362d4e3d0cc0
carlosp420 <mycalesis@gmail.com>
2015-11-04 15:45:05 +0200

    correct error messages

--- voseq/create_dataset/tests/create_mega_dataset/tests_create_mega_dataset.py
+++ voseq/create_dataset/tests/create_mega_dataset/tests_create_mega_dataset.py
@@ -52,13 +52,15 @@ class CreateMegaDatasetTest(TestCase):
         dataset_creator = CreateDataset(cleaned_data)
 
         result = dataset_creator.dataset_str
-        self.assertEqual(expected.strip(), result)
+        self.assertEqual(expected, result)
 
     def test_all_codons_partitioned_as_each(self):
+        g1 = Genes.objects.get(gene_code='COI-begin')
         cleaned_data = self.cleaned_data.copy()
+        cleaned_data['gene_codes'] = [g1]
         cleaned_data['positions'] = ['ALL']
         cleaned_data['partition_by_positions'] = 'by codon position'
         dataset_creator = CreateDataset(cleaned_data)
-        expected = 'Cannot produce MEGA dataset with codon positions in different partitions.'
+        expected = 'Cannot produce MEGA dataset with codon positions in different partitions'
         result = dataset_creator.errors
-        self.assertTrue(expected in result)
+        self.assertEqual(expected, str(result[0]))
