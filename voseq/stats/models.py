ed439b462122fad12a802c1a6ef8f739fd0e6b3d
Carlos Peña <mycalesis@gmail.com>
2015-11-12 15:02:01 +0200

    more __str__

--- voseq/stats/models.py
+++ voseq/stats/models.py
@@ -15,9 +15,15 @@ class Stats(models.Model):
     species = models.IntegerField()
     sequences = models.IntegerField()
 
+    def __str__(self):
+        return "{0} vouchers".format(self.vouchers)
+
 
 class VouchersPerGene(models.Model):
     """Number of vouchers that have sequences for each of our genes.
     """
     gene_code = models.CharField(max_length=100)
     voucher_count = models.IntegerField()
+
+    def __str__(self):
+        return "{0} vouchers in gene {1}".format(self.voucher_count, self.gene_code)
