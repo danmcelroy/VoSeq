26fa0aba728dbdfc9cf352b0e4a224a06591c871
carlosp420 <mycalesis@gmail.com>
2016-09-11 00:05:05 -0500

    fixed templates

--- voseq/voseq/settings/base.py
+++ voseq/voseq/settings/base.py
@@ -79,9 +79,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, '..', 'templates'),
-            os.path.join(BASE_DIR, '..', 'core', 'templates'),
+            'templates',
         ],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
