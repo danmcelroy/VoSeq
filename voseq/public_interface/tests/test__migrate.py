98239d907721faee22eecb294c39b86582065bea
carlosp420 <mycalesis@gmail.com>
2017-03-07 21:44:27 -0500

    many fixes to migrate db

---
+++ voseq/public_interface/tests/test__migrate.py
@@ -0,0 +1,10 @@
+import unittest
+
+from public_interface.management.commands._migrate_db import get_as_tuple
+
+
+class Test_Migrate(unittest.TestCase):
+    def test_get_as_tuple(self):
+        my_string = "|14799231204"
+        result = get_as_tuple(my_string)
+        self.assertEqual("", result)
