43a1654174a111a7381364cf9978fdccd7ce0fc0
Carlos Peña <mycalesis@gmail.com>
2017-04-09 20:12:49 -0500

    added haystack conf to testing settings

--- voseq/voseq/settings/testing.py
+++ voseq/voseq/settings/testing.py
@@ -1,6 +1,6 @@
 import sys
 
-from .local import *
+from .base import *
 
 PHOTOS_REPOSITORY = "local"
 ELASTICSEARCH = True
@@ -15,3 +15,51 @@ DATABASES = {
         'NAME': 'test.db',
     }
 }
+
+DB_NAME = "test"
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': '{}_haystack'.format(DB_NAME),
+        'INCLUDE_SPELLING': True,
+        'EXCLUDED_INDEXES': [
+            'public_interface.search_indexes.AdvancedSearchIndex',
+            'public_interface.search_indexes.AutoCompleteIndex',
+            'public_interface.search_indexes.VouchersIndex',
+        ],
+    },
+    'autocomplete': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': '{}_autocomplete'.format(DB_NAME),
+        'INCLUDE_SPELLING': False,
+        'EXCLUDED_INDEXES': [
+            'public_interface.search_indexes.SimpleSearchIndex',
+            'public_interface.search_indexes.VouchersIndex',
+        ],
+    },
+    'vouchers': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': '{}_vouchers'.format(DB_NAME),
+        'INCLUDE_SPELLING': False,
+        'EXCLUDED_INDEXES': [
+            'public_interface.search_indexes.SimpleSearchIndex',
+            'public_interface.search_indexes.AdvancedSearchIndex',
+            'public_interface.search_indexes.AutoCompleteIndex',
+        ],
+    },
+    'advanced_search': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': '{}_advanced_search'.format(DB_NAME),
+        'INCLUDE_SPELLING': False,
+        'EXCLUDED_INDEXES': [
+            'public_interface.search_indexes.SimpleSearchIndex',
+            'public_interface.search_indexes.VouchersIndex',
+        ],
+    },
+}
+HAYSTACK_DEFAULT_OPERATOR = 'AND'
