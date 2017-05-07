b4a282bb2e728b4b2d0ccfa9b7506d62c7483913
carlosp420 <mycalesis@gmail.com>
2017-05-07 02:13:06 -0500

    fixed test saving data

--- voseq/public_interface/tests/test_saving_data.py
+++ voseq/public_interface/tests/test_saving_data.py
@@ -1,5 +1,4 @@
 from django.core.management import call_command
-from django.db import connection
 from django.test import TestCase
 
 from public_interface.models import Sequences
@@ -13,19 +12,19 @@ class TestViews(TestCase):
         cmd = 'migrate_db'
         call_command(cmd, *args, **opts)
 
-        self.voucher_model = Vouchers.objects.filter(code='CP100-18')[0]
+        self.voucher_model = Vouchers.objects.get(code='CP100-18')
+        self.maxDiff = None
 
     def test_save_sequences_ambiguous_characters(self):
-        sequence_model = Sequences(
+        sequence_model = Sequences.objects.get(
             code=self.voucher_model,
-            sequences='???---NNNATCTACTA',
             gene_code='COI',
-            genbank=False,
         )
+        sequence_model.sequences = '???---NNNATCTACTA'
         sequence_model.save()
 
-        sequence_model = Sequences.objects.filter(
+        sequence_model = Sequences.objects.get(
             code=self.voucher_model,
             gene_code='COI',
-        )[0]
-        self.assertEqual(sequence_model.number_ambiguous_bp, 25)
+        )
+        self.assertEqual(sequence_model.number_ambiguous_bp, 9)
