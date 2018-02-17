744e302d0d616865e34cc7316077a7f88be19b13
carlosp420 <mycalesis@gmail.com>
2018-02-17 10:28:32 -0500

    fixes to blast_local.views

--- voseq/blast_local/views.py
+++ voseq/blast_local/views.py
@@ -1,17 +1,20 @@
 import logging
 from subprocess import CalledProcessError
 
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from core.utils import get_version_stats
-from core.utils import get_username
-from core.utils import BLAST
+from core.utils import get_version_stats, get_username, BLAST
 
 
 log = logging.getLogger(__name__)
 
 
-def index(request, voucher_code, gene_code):
+def index(request: HttpRequest, voucher_code: str, gene_code: str) -> HttpResponse:
+    """Runs a local blast for voucher code and gene code
+
+    Show results to user in a table
+    """
     version, stats = get_version_stats()
     username = get_username(request)
 
@@ -22,15 +25,17 @@ def index(request, voucher_code, gene_code):
         try:
             blast.create_blast_db()
         except CalledProcessError:
-            log.warning("there are no sequences for gene {}".format(gene_code))
+            log.warning("there are no sequences for gene %s", gene_code)
 
-    good_sequence = blast.save_query_to_file()
-    if good_sequence:
+    was_sequence_saved = blast.save_query_to_file()
+    if was_sequence_saved:
         blast.do_blast()
         result = blast.parse_blast_output()
         blast.delete_query_output_files()
     else:
-        result = {"error": "Query sequence has no valid codons, only question marks"}
+        result = {
+            "error": "Query sequence has no valid codons, only question marks",
+        }
     return render(
         request, 'blast_local/index.html',
         {
