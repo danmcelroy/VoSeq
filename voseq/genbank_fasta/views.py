f6db6acc4d48320cc74aba5c6b456aa3dbd8c0f7
carlosp420 <mycalesis@gmail.com>
2018-02-18 09:55:20 -0500

    rolled back

--- voseq/genbank_fasta/views.py
+++ voseq/genbank_fasta/views.py
@@ -32,10 +32,21 @@ def results(request):
         form = GenBankFastaForm(request.POST)
 
         if form.is_valid():
-            cleaned_data = fix_cleaned_data(form)
+            cleaned_data = form.cleaned_data
+            cleaned_data['file_format'] = 'GenBankFASTA'
+            cleaned_data['number_genes'] = ''
+            cleaned_data['translations'] = False
+            cleaned_data['aminoacids'] = False
+            cleaned_data['positions'] = 'ALL'
+            cleaned_data['partition_by_positions'] = 'by gene'
+            cleaned_data['taxon_names'] = ['CODE', 'GENUS', 'SPECIES']
+            cleaned_data['outgroup'] = ''
+
             dataset_creator = CreateDataset(cleaned_data)
             dataset = dataset_creator.dataset_str
             dataset_short = dataset[0:1500] + '\n...\n\n\n' + '#######\nComplete dataset file available for download.\n#######'  # noqa
+            errors = dataset_creator.errors
+            warnings = dataset_creator.warnings
             dataset_file_abs = dataset_creator.dataset_file
             items_with_accession = dataset_creator.sequences_skipped
             if dataset_file_abs is not None:
@@ -56,33 +67,27 @@ def results(request):
             context['dataset'] = dataset_short
             context['fasta_file'] = dataset_file
             context['protein'] = aa_dataset
-            context['errors'] = dataset_creator.errors
+            context['errors'] = errors
             context['protein_file'] = aa_dataset_file
-            context['warnings'] = dataset_creator.warnings
-        context["form"] = form
-        return render(request, 'genbank_fasta/index.html', context)
+            context['warnings'] = warnings
+            return render(request, 'genbank_fasta/results.html', context)
+        else:
+            context["form"] = form
+            return render(request, 'genbank_fasta/index.html', context)
 
     return HttpResponseRedirect('/genbank_fasta/')
-
-
-def fix_cleaned_data(form):
-    cleaned_data = form.cleaned_data
-    cleaned_data['file_format'] = 'GenBankFASTA'
-    cleaned_data['number_genes'] = ''
-    cleaned_data['translations'] = False
-    cleaned_data['aminoacids'] = False
-    cleaned_data['positions'] = 'ALL'
-    cleaned_data['partition_by_positions'] = 'by gene'
-    cleaned_data['taxon_names'] = ['CODE', 'GENUS', 'SPECIES']
-    cleaned_data['outgroup'] = ''
-    return cleaned_data
 
 
 @login_required
 def serve_file(request, file_name):
     log.debug("Requested file by user: {0}".format(request.user))
     cwd = os.path.dirname(__file__)
-    fasta_file = os.path.join(cwd, '..', 'create_dataset', 'dataset_files', file_name)
+    fasta_file = os.path.join(cwd,
+                              '..',
+                              'create_dataset',
+                              'dataset_files',
+                              file_name,
+                              )
     response = HttpResponse(open(fasta_file, 'r').read(), content_type='application/text')
     response['Content-Disposition'] = 'attachment; filename=voseq_genbank.fasta'
 
