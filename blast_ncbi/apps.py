e9e66382f3115e01b55c3ca16fb4cdff0cbf6f95
Carlos Pena <carlos@hiplead.com>
2020-04-04 19:12:00 -0500

    fixes

---
+++ blast_ncbi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BlastNcbi(AppConfig):
+    name = 'blast_ncbi'
---
+++ gbif/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Gbif(AppConfig):
+    name = 'gbif'
