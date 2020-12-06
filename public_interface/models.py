dea74af36262f39c0006e3cf45fd1791ed12ab78
Carlos Peña <mycalesis@gmail.com>
2020-12-06 09:42:06 -0500

    added warning

--- public_interface/models.py
+++ public_interface/models.py
@@ -232,6 +232,9 @@ class Sequences(models.Model):
         app_label = 'public_interface'
 
     def save(self, *args, **kwargs):
+        if not self.gene:
+            raise Exception(f'It is necessary to provide a value for column `gene`.')
+        
         ambiguous_seq_length = self.sequences.count('?') + self.sequences.count('-')
         ambiguous_seq_length += self.sequences.count('N') + self.sequences.count('n')
         self.number_ambiguous_bp = ambiguous_seq_length
