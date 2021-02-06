26797343a298aed643e454a4f84cac031b851d0f
Carlos Peña <mycalesis@gmail.com>
2021-02-06 14:35:27 -0500

    fixes

--- voseq/settings/local.py
+++ voseq/settings/local.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ImproperlyConfigured
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
 MANAGERS = ()
